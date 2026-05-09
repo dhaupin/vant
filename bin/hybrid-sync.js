8f08940a8b5e0c6ae0fbe82c1922c4ce59976804
dhaupin <dhaupin@gmail.com>
2026-05-09 13:14:42 +0000

    Fix: hybrid-sync.js use sync module

--- bin/hybrid-sync.js
+++ bin/hybrid-sync.js
@@ -27,7 +27,7 @@ const action = (argsSet.has('-p') || argsSet.has('--public')) ? 'public' :
 
 const path = require('path');
 const DIR = path.join(__dirname, '..');
-const hybrid = require(path.join(DIR, 'lib', 'hybrid-sync'));
+const hybrid = require('./sync');
 
 async function run() {
     if (!action) {
